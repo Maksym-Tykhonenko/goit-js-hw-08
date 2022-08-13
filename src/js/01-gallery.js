727692d5294c56841293364e6d1cb35892d47658
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2022-08-13 20:53:16 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-
 import SimpleLightbox from 'simplelightbox';
 import "SimpleLightbox/dist/simple-lightbox.min.css";
 // Change code below this line
